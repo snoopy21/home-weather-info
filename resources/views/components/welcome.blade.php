c3df9b48198b3f7f0f7cb2a12a6252c4b9745deb
martin <martin@vilan.ch>
2023-06-05 00:12:07 +0200

    Dashboard Struktur angepasst inkl. Icons

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -15,76 +15,65 @@
 <div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg>
-            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold"><a href="https://laravel.com/docs">Documentation</a></div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-blue-900 dark:text-blue-400"><polyline class="cls-1" points="22.5 10.57 12 1.98 1.5 10.57"/><path class="cls-1" d="M13.91,17V9.61a1.91,1.91,0,0,0-3.82,0V17a2.86,2.86,0,1,0,3.82,0Z"/><line class="cls-1" x1="15.82" y1="10.57" x2="13.91" y2="10.57"/><line class="cls-1" x1="15.82" y1="14.39" x2="13.91" y2="14.39"/><polyline class="cls-1" points="7.23 22.02 4.36 22.02 4.36 8.66"/><polyline class="cls-1" points="16.77 22.02 19.64 22.02 19.64 8.66"/></svg>
+            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold">Stube</div>
         </div>
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
+                <ul class="leading-loose">
+                    <li>Temp.:</li>
+                    <li>rel.-Luftfeuchtigkeit:</li>
+                </ul>
             </div>
 
-            <a href="https://laravel.com/docs">
-                <div class="mt-3 flex items-center text-sm font-semibold text-blue-700">
-                    <div>Explore the documentation</div>
-
-                    <div class="ml-1 text-blue-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                        </svg>
-                    </div>
-                </div>
-            </a>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" /></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="https://laracasts.com">Laracasts</a></div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-blue-900 dark:text-blue-400"><path d="M11 15C12.1046 15 13 15.8954 13 17C13 18.1046 12.1046 19 11 19C9.89543 19 9 18.1046 9 17C9 15.8954 9.89543 15 11 15ZM11 15V8M15 6H18M15 8H18M15 10H18M15 12H18M11 3C12.1046 3 13 3.89543 13 5L13.0011 13.5358C14.1961 14.2275 15 15.5199 15 17C15 19.2091 13.2091 21 11 21C8.79086 21 7 19.2091 7 17C7 15.5195 7.80434 14.2268 8.99988 13.5352L9 5C9 3.89543 9.89543 3 11 3Z" stroke-linejoin="round"/></svg>
+            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold">draussen</div>
         </div>
 
         <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500 dark:text-gray-100">
-                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+            <div class="mt-2 text-sm text-gray-500 dark:text-gray-400">
+                <ul class="leading-loose">
+                    <li>Temp.:</li>
+                    <li>rel.-Luftfeuchtigkeit:</li>
+                </ul>
             </div>
-
-            <a href="https://laracasts.com">
-                <div class="mt-3 flex items-center text-sm font-semibold text-blue-700 dark:text-blue-300">
-                    <div>Start watching Laracasts</div>
-
-                    <div class="ml-1 text-blue-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                        </svg>
-                    </div>
-                </div>
-            </a>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 dark:border-gray-700">
         <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" /></svg>
-            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold"><a href="https://tailwindcss.com/">Tailwind</a></div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-blue-900 dark:text-blue-400"><path d="M 0.625,3 C 0.25,3 0,3.25 0,3.75 l 0,7.25 1.25,0 0,-1.75 c 0,0 10.380697,0.01892 11.5,0 L 12.75,11 14,11 14,8.625 14,4.75 C 14,4.4202582 13.86199,4 13.375,4 12.88801,4 12.75,4.4202582 12.75,4.75 l 0,3.25 -11.5,0 0,-4.25 C 1.25,3.25 1,3 0.625,3 z M 3.5,4 C 2.671573,4 2,4.671573 2,5.5 2,6.328427 2.671573,7 3.5,7 4.328427,7 5,6.328427 5,5.5 5,4.671573 4.328427,4 3.5,4 z m 2,1 0,2 6.5,0 C 12,6 10.963825,5 10,5 z" /></svg>
+            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold">Kinderzimmer</div>
         </div>
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
+                <ul class="leading-loose">
+                    <li>Temp.:</li>
+                    <li>rel.-Luftfeuchtigkeit:</li>
+                </ul>
             </div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
         <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400"><path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" /></svg>
-            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold">Authentication</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-blue-900 dark:text-blue-400"><path d="M96 77.3c0-7.3 5.9-13.3 13.3-13.3c3.5 0 6.9 1.4 9.4 3.9l14.9 14.9C130 91.8 128 101.7 128 112c0 19.9 7.2 38 19.2 52c-5.3 9.2-4 21.1 3.8 29c9.4 9.4 24.6 9.4 33.9 0L289 89c9.4-9.4 9.4-24.6 0-33.9c-7.9-7.9-19.8-9.1-29-3.8C246 39.2 227.9 32 208 32c-10.3 0-20.2 2-29.2 5.5L163.9 22.6C149.4 8.1 129.7 0 109.3 0C66.6 0 32 34.6 32 77.3V256c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H96V77.3zM32 352v16c0 28.4 12.4 54 32 71.6V480c0 17.7 14.3 32 32 32s32-14.3 32-32V464H384v16c0 17.7 14.3 32 32 32s32-14.3 32-32V439.6c19.6-17.6 32-43.1 32-71.6V352H32z"/></svg>
+            <div class="ml-4 text-lg text-gray-600 dark:text-gray-100 leading-7 font-semibold">Badezimmer</div>
         </div>
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started what matters most: building your application.
+                <ul class="leading-loose">
+                    <li>Temp.:</li>
+                    <li>rel.-Luftfeuchtigkeit:</li>
+                </ul>
             </div>
         </div>
     </div>
